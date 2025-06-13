73da92f8445dad47ad0d80e799482d07808b4a02
purojinshamini <purojinshamini@gmail.com>
2025-06-14 00:07:40 +0530

    footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { HomeBlockComponent } from "./home-block/home-block.component";
+import { FooterBlockComponent } from './footer-block/footer-block.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HomeBlockComponent],
+  imports: [HomeBlockComponent,
+    FooterBlockComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
