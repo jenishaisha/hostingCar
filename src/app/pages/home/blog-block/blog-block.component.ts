f6858d18c75ce5b1ca802abdd14bbdd95c733f51
purojinshamini <purojinshamini@gmail.com>
2025-06-16 20:56:42 +0530

    home change

---
+++ src/app/pages/home/blog-block/blog-block.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-block',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-block.component.html',
+  styleUrl: './blog-block.component.css'
+})
+export class BlogBlockComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { HomeBlockComponent } from "./home-block/home-block.component";
 import { FooterBlockComponent } from './footer-block/footer-block.component';
+import { BlogBlockComponent } from "./blog-block/blog-block.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [HomeBlockComponent,
-    FooterBlockComponent
-  ],
+    FooterBlockComponent, BlogBlockComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
