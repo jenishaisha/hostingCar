ce860d383180e5600b3e4da35970b8606d69bd51
jebinjack2000 <jebinjack2000@gmail.com>
2025-06-17 00:22:04 +0530

    service and about us page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { HomeBlockComponent } from "./home-block/home-block.component";
 import { FooterBlockComponent } from './footer-block/footer-block.component';
 import { BlogBlockComponent } from "./blog-block/blog-block.component";
+import { AboutUsComponent } from '../../about-us/about-us.component';
+import { ServiceComponent } from '../../../service/service.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [HomeBlockComponent,
-    FooterBlockComponent, BlogBlockComponent],
+    FooterBlockComponent, BlogBlockComponent,AboutUsComponent,ServiceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
