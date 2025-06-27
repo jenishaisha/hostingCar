72477c7bbe6d9f7362be3d8885b6845a46be3b03
jebinjack2000 <jebinjack2000@gmail.com>
2025-06-27 17:06:01 +0530

    routerlink

--- src/app/pages/home/blog-block/blog-block.component.ts
+++ src/app/pages/home/blog-block/blog-block.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog-block',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './blog-block.component.html',
   styleUrl: './blog-block.component.css'
 })
--- src/service/service.component.ts
+++ src/service/service.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-service',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './service.component.html',
   styleUrl: './service.component.css'
 })
