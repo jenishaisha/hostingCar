3679a54bafc4197df8c1fb96f451e54fc9811db1
purojinshamini <purojinshamini@gmail.com>
2025-07-06 17:31:21 +0530

    Responsive fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideIcons } from '@ng-icons/core';
+import { bootstrapList } from '@ng-icons/bootstrap-icons';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(),
+    provideIcons({ bootstrapList })
+  ]
 };
